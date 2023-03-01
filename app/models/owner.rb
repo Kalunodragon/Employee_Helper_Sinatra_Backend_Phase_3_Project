d7961c49a0abc41f7408fda47afe5ef525e644d4
Andrew <andrewonulak@gmail.com>
2023-02-28 23:33:39 -0700

    Added some instance methods for seeing how many peices an owner has and how many different themes they own

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,20 @@
 class Owner < ActiveRecord::Base
     has_many :notes
     has_many :lego_sets, through: :notes
+
+    def total_lego_peices
+        total = 0
+        self.lego_sets.each do |set|
+            total += set.peices
+        end
+        total
+    end
+
+    def owned_themes
+        array = []
+        self.lego_sets.each do |e|
+            array << e.theme unless array.include?(e.theme)
+        end
+        array
+    end
 end
